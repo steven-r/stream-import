07464d461b2ee2288fdfd597169eedb3e7537317
Stephen Reindl <stephen@stephenreindl.net>
2015-07-27 20:48:35 +0200

    fix failing test for de-DE

--- CsvImporterTests/CsvTests.cs
+++ CsvImporterTests/CsvTests.cs
@@ -282,12 +282,10 @@ namespace ImporterTests
             CultureInfo info = new CultureInfo("de-CH");
             Assert.Equal("de-CH", info.Name);
             info.TextInfo.ListSeparator = ";";
-
 #if FIX_APVOYER
             info.NumberFormat.NumberDecimalSeparator = ".";
             info.NumberFormat.NumberGroupSeparator = "'";
 #endif
-
             string data = "Test1;Test3;\"Test\";\nData1;Data2;1.0\nData1;Data2;2.0\nData1;Data2\nData1;Data2;2.43\n";
             CsvStreamImporter streamImporter = new CsvStreamImporter(GenerateStreamFromString(data), info);
             streamImporter.SetHeader(true);
@@ -309,10 +307,6 @@ namespace ImporterTests
         {
             CultureInfo info = new CultureInfo("de-DE");
             Assert.Equal("de-DE", info.Name);
-#if FIX_APVOYER
-            info.NumberFormat.NumberDecimalSeparator = ".";
-            info.NumberFormat.NumberGroupSeparator = "'";
-#endif
             info.TextInfo.ListSeparator = ";";
 
             string data = "Test1;Test3;\"Test\";\nData1;Data2;1,0\nData1;Data2;2,0\nData1;Data2\nData1;Data2;2,43\n";
